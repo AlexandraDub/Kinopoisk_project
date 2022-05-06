2cb529d76661f2ebb991555e31ad6ce7d6b7b41b
Alexandra Dubovskaya <alyaksandradub@gmail.com>
2022-05-06 20:19:08 +0300

    commit2

--- JS/main_page.js
+++ JS/main_page.js
@@ -1,31 +1,71 @@
 const filmsImagesContainer = document.querySelector('.films')
+const filmNumber = 6
+
+let filmDataArr = []
+
+const signInButton = document.querySelector('#login')
+signInButton.addEventListener('click', () => sortFilms(sortByReleaseDateDesc))
 
 async function loadInfo() {
-    for (let i = 1; i <= 6; i++) {
+
+    for (let i = 1; i <= filmNumber; i++) {
         let response = await fetch(`https://swapi.dev/api/films/${i}/`);
         let result = await response.json();
         console.log(result);
-        filmsRender(result)
-        function filmsRender(result) {
-
-            const episode = document.createElement('div')
-            const infoContainer = document.createElement('div')
-            episode.style.backgroundImage = `url(../Images/ep${i}.png)`;
-            episode.classList.add('episode')
-            infoContainer.classList.add('films_info')
-            episode.id = `ep${i}`
-            infoContainer.innerHTML = `${result.title}, ${result.episode_id}, ${result.release_date}`
-            filmsImagesContainer.append(episode)
-            episode.append(infoContainer)
-        
-        
-        
-        }
+        filmDataArr.push(result)
     }
 
+    filmsRender()
+}
+
+function filmsRender() {
+
+filmsImagesContainer.innerHTML = ''
+
+    for (let i = 1; i <= filmNumber; i++) {
+
+        const filmData = filmDataArr[i - 1]
+        const episode = document.createElement('div')
+        const infoContainer = document.createElement('div')
+        episode.style.backgroundImage = `url(../Images/ep${filmData.episode_id}.png)`;
+        episode.classList.add('episode')
+        infoContainer.classList.add('films_info')
+        //episode.id = `ep${i}`
+        infoContainer.innerHTML = `<div>Episode ${filmData.episode_id}</div><div>${filmData.title}</div> <div>Release date:${filmData.release_date}</div>`
+        filmsImagesContainer.append(episode)
+        episode.append(infoContainer)
+
+    }
 
 }
+
+// Sort comparing functions 
+function sortByEpisodeAsc(filmData1, filmData2){
+    return filmData1.episode_id - filmData2.episode_id
+}
+
+function sortByEpisodeDesc(filmData1, filmData2){
+    return filmData2.episode_id - filmData1.episode_id
+}
+
+function sortByReleaseDateAsc(filmData1, filmData2){
+    return Date.parse(filmData1.release_date) - Date.parse(filmData2.release_date)
+}
+
+function sortByReleaseDateDesc(filmData1, filmData2){
+    return Date.parse(filmData2.release_date) - Date.parse(filmData1.release_date)
+}
+
+function sortFilms(comparator) {
+    filmDataArr.sort(comparator)
+    filmsRender()
+}
+
+
+
 loadInfo()
+
+
 
 
 
